f25e1d4af500184d69193f2103d417c60ffa37f2
Shivain Thapar <shivain27@gmail.com>
2017-12-21 13:47:40 -0500

    simple component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-// new component that produces some html
-// import * as React from "react";
+import React from 'react';
+import ReactDom from 'react-dom';
 
-const App = function () {
-	return <div>hi!</div>; // jsx right here baby 
+// new component that produces some html
+const App = () => {
+	return <div>hi!</div>; // jsx right here baby
 };
 
 // take this component's html and put it on the page
-// in the DOM
-
+// (in the DOM)
+ReactDom.render(<App/>, document.querySelector('.container'));
