9c6c6184cb855d9d7a5fe543cf5a262722e7ba6e
sharpar <shivain27@gmail.com>
2017-12-22 23:06:08 -0500

    Finish section 1 of course, add component with data from YouTube API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchBar';
 
 const API_KEY = 'AIzaSyC-SOc0Fz13PNhOsvIOyfAF7BLBdmSsV2c';
 
-// new component that produces some html
-const App = () => {
-	return <div>
-		<SearchBar />
-	</div>;                        
-};
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({ key: API_KEY, term: 'lakers' }, videos => {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return <div>
+			<SearchBar/>
+		</div>;
+	}
+}
 
 // take this component's html and put it on the page
 // (in the DOM)
